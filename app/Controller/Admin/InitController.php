dbb8d21732c62ad7b60c126ef985efdf320ba345
nicaine <1185802277@qq.com>
2021-05-31 14:52:56 +0800

    init

--- app/Controller/Admin/InitController.php
+++ app/Controller/Admin/InitController.php
@@ -3,12 +3,13 @@
 
 namespace App\Controller\Admin;
 
-use App\Model\Admin\Menu;
-use App\Model\Core\TemplateSet;
+use App\Model\AdminSet;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\RequestMapping;
 use Hyperf\HttpServer\Annotation\Middleware;
 use App\Middleware\IsLoginMiddleware;
+use App\Model\Menu;
+use App\Service\Base;
 
 /**
  * @Controller()
@@ -19,46 +20,55 @@ class InitController extends AbstractController
      * @RequestMapping(path="/admin/init/list", methods="get")
      * @Middleware(IsLoginMiddleware::class)
      */
-    public function list()
+    public function list(Base $base,Menu $menus)
     {
-        $allAuth = $this->helper->getAdminAuth();
+        $allAuth = array_unique($base->getAdminAuth());
 
-        $menu = Menu::with('child')
-            ->where('status','1')
-            ->where('pid','0')
-            ->whereIn('id',$allAuth)
-            ->get();
+        $menu =$menus->getLevel();
+        $k1=0;
+        $k2=0;
+        $k3=0;
         $list=array();
-        foreach ($menu as $k=>$v){
-            $list[$k]['title']=$v->name;
-            $list[$k]['icon']=$v->icon;
-            $list[$k]['href']="";
-            $list[$k]['target']="_self";
-            foreach ($v->child as $ik=>$item){
-                $list[$k]['child'][$ik]['title']=$item->name;
-                $list[$k]['child'][$ik]['icon']=$item->icon;
-                $list[$k]['child'][$ik]['href']='';
-                $list[$k]['child'][$ik]['target']='_self';
-                foreach ($item->child as $vk=>$val){
-                    $list[$k]['child'][$ik]['child'][$vk]['title']=$val->name;
-                    $list[$k]['child'][$ik]['child'][$vk]['icon']=$val->icon;
-                    $list[$k]['child'][$ik]['child'][$vk]['href']=$val->url;
-                    $list[$k]['child'][$ik]['child'][$vk]['target']=$val->_self;
+        foreach ($menu as $k=>$v) {
+            if($v->status===1 && in_array($v->id,$allAuth)){
+                $list[$k1]['title'] = $v->name;
+                $list[$k1]['icon'] = $v->icon;
+                $list[$k1]['href'] = "";
+                $list[$k1]['target'] = "_self";
+                foreach ($v->child as $ik => $item) {
+                    if($item->status===1 && in_array($item->id,$allAuth)){
+                        $list[$k1]['child'][$k2]['title'] = $item->name;
+                        $list[$k1]['child'][$k2]['icon'] = $item->icon;
+                        $list[$k1]['child'][$k2]['href'] = '';
+                        $list[$k1]['child'][$k2]['target'] = '_self';
+                        foreach ($item->child as $vk => $val) {
+                            if($val->status===1 && in_array($val->id,$allAuth)) {
+                                $list[$k1]['child'][$k2]['child'][$k3]['title'] = $val->name;
+                                $list[$k1]['child'][$k2]['child'][$k3]['icon'] = $val->icon;
+                                $list[$k1]['child'][$k2]['child'][$k3]['href'] = $val->url;
+                                $list[$k1]['child'][$k2]['child'][$k3]['target'] = $val->_self;
+                                $k3++;
+                            }
+                        }
+                        $k2++;
+                    }
                 }
+                $k1++;
+                $k2=0;
+                $k3=0;
             }
         }
-        $setList = TemplateSet::query()->where('key','=','basic')->first();
-        $set = json_decode($setList->val);
+        $set = AdminSet::query()->where('key','=','logo')->first();
 
         $init = array(
             'homeInfo'=>[
                 'title'=>'首页',
-                'href'=>'page/welcome-1.html?t=1'
+                'href'=>'/aaaa'
             ],
             'logoInfo'=>[
-                'title'=>'',
-                'image'=>$set->logo,
-                'href'=>''
+                'title'=>'管理系统',
+                'image'=>$set->val ?? '',
+                'href'=>'/admin/index/layout'
             ],
             'menuInfo'=>$list
         );
